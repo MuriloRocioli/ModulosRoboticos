a17d27ef60d0b2797ebf2d5420664f85d9269e74
MuriloRocioli <82733516+MuriloRocioli@users.noreply.github.com>
2024-08-30 09:34:08 -0300

    Add files via upload

---
+++ LoRa/LoRa_Stock_Novo/LoRa_Stock_Controle/LoRa_Stock_Controle.ino
@@ -0,0 +1,166 @@
+/* 
+*   Projeto CELESC - UFU
+*   Codigo referente ao controle do Robo para remocao de Detritos
+*
+*   Autor: Murilo Rocioli
+*/
+
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+#include <Wire.h>  
+#include "HT_SSD1306Wire.h"
+
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#define TX_OUTPUT_POWER                             5        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 30 // Define the payload size here
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+double txNumber;
+
+bool lora_idle=true;
+
+// Definições dos pinos dos botões
+const byte PIN_BUTTON_A = 22;
+const byte PIN_BUTTON_B = 23;
+const byte PIN_BUTTON_C = 0;
+const byte PIN_BUTTON_D = 17;
+
+const byte PIN_ANALOG_X = 12;
+const byte PIN_ANALOG_Y = 13;
+
+static RadioEvents_t RadioEvents;
+void OnTxDone( void );
+void OnTxTimeout( void );
+
+SSD1306Wire  factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+
+void setup() {
+    Serial.begin(115200);
+    Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+
+    factory_display.init();
+	  factory_display.clear();
+	  factory_display.display();
+    factory_display.drawString(0, 10, "CELESC UFU");
+    factory_display.drawString(0, 20, "Robo Remoção Detritos");
+	  delay(300);
+	  factory_display.clear();
+  
+    pinMode(PIN_BUTTON_A, INPUT);
+    digitalWrite(PIN_BUTTON_A, HIGH);
+
+    pinMode(PIN_BUTTON_B, INPUT);
+    digitalWrite(PIN_BUTTON_B, HIGH);
+
+    pinMode(PIN_BUTTON_C, INPUT);
+    digitalWrite(PIN_BUTTON_C, HIGH);
+
+    pinMode(PIN_BUTTON_D, INPUT);
+    digitalWrite(PIN_BUTTON_D, HIGH);
+	
+    txNumber=0;
+
+    RadioEvents.TxDone = OnTxDone;
+    RadioEvents.TxTimeout = OnTxTimeout;
+    
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                                   LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                                   LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                   true, 0, 0, LORA_IQ_INVERSION_ON, 3000 ); 
+   }
+
+
+
+void loop()
+{
+	if (lora_idle == true) {
+        delay(1000);
+        
+        int buttonStateA = digitalRead(PIN_BUTTON_A);
+        int buttonStateB = digitalRead(PIN_BUTTON_B);
+        int buttonStateC = digitalRead(PIN_BUTTON_C);
+        int buttonStateD = digitalRead(PIN_BUTTON_D);
+        int buttonStateX = analogRead(PIN_ANALOG_X);
+        int buttonStateY = analogRead(PIN_ANALOG_Y);
+
+        if (buttonStateA == LOW) {
+            sprintf(txpacket, "A");
+        } else {
+            sprintf(txpacket, "Aguardando...");
+        }
+        
+        if (buttonStateB == LOW) {
+            sprintf(txpacket, "B");
+        }
+
+        if (buttonStateC == LOW) {
+            sprintf(txpacket, "C");
+        }
+
+        if (buttonStateD == LOW) {
+            sprintf(txpacket, "D");
+        }
+
+        if (buttonStateX >= 2700) {
+            sprintf(txpacket, "Avancar");
+        }
+
+        if (buttonStateX <= 100) {
+            sprintf(txpacket, "Recuar");
+        }
+
+        Serial.print("X:");
+        Serial.print(analogRead(PIN_ANALOG_X));
+        Serial.print(" ");
+
+        Serial.print("Y:");
+        Serial.print(analogRead(PIN_ANALOG_Y));
+        Serial.print(" ");
+        
+
+        Serial.printf("\r\nSending packet \"%s\" , length %d\r\n", txpacket, strlen(txpacket));
+        factory_display.drawString(0, 10, txpacket);
+        factory_display.display();
+        delay(10);
+        factory_display.clear();
+        Radio.Send((uint8_t *)txpacket, strlen(txpacket)); // Send the package out    
+        lora_idle = false;
+    }
+  Radio.IrqProcess( );
+}
+
+void OnTxDone( void )
+{
+	Serial.println("TX done......");
+	lora_idle = true;
+}
+
+void OnTxTimeout( void )
+{
+    Radio.Sleep( );
+    Serial.println("TX Timeout......");
+    lora_idle = true;
+}
---
+++ LoRa/LoRa_Stock_Novo/LoRa_Stock_Robo/LoRa_Stock_Robo.ino
@@ -0,0 +1,263 @@
+/* 
+*   Projeto CELESC - UFU
+*   Codigo referente ao Robo para remocao de Detritos
+*
+*   Autor: Murilo Rocioli
+*/
+
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 30 // Define the payload size here
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+
+int16_t txNumber;
+
+int16_t rssi,rxSize;
+
+bool lora_idle = true;
+
+#define MOTOR_A_PIN1 23
+#define MOTOR_A_PIN2 17
+
+#define MOTOR_B_PIN1 0
+#define MOTOR_B_PIN2 22
+
+#define MOTOR_C_PIN1 12
+#define MOTOR_C_PIN2 13
+
+#define MOTOR_D_PIN1 34
+#define MOTOR_D_PIN2 35
+
+
+
+bool com_recuar = 0;
+bool com_avancar = 0;
+
+void setup() {
+    Serial.begin(115200);
+    Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+
+    pinMode(MOTOR_A_PIN1, OUTPUT);
+    pinMode(MOTOR_A_PIN2, OUTPUT);
+
+    pinMode(MOTOR_B_PIN1, OUTPUT);
+    pinMode(MOTOR_B_PIN2, OUTPUT);
+
+    pinMode(MOTOR_C_PIN1, OUTPUT);
+    pinMode(MOTOR_C_PIN2, OUTPUT);
+
+    pinMode(MOTOR_D_PIN1, OUTPUT);
+    pinMode(MOTOR_D_PIN2, OUTPUT);
+    
+    digitalWrite(MOTOR_A_PIN1, LOW);
+    digitalWrite(MOTOR_A_PIN2, LOW);
+
+    digitalWrite(MOTOR_B_PIN1, LOW);
+    digitalWrite(MOTOR_B_PIN2, LOW);
+
+    digitalWrite(MOTOR_C_PIN1, LOW);
+    digitalWrite(MOTOR_C_PIN2, LOW);
+
+    digitalWrite(MOTOR_D_PIN1, LOW);
+    digitalWrite(MOTOR_D_PIN2, LOW);
+    
+    txNumber=0;
+    rssi=0;
+  
+    RadioEvents.RxDone = OnRxDone;
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                               LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                               LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                               0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+}
+
+
+
+void loop()
+{
+  if(lora_idle)
+  {
+    lora_idle = false;
+    Serial.println("into RX mode");
+    Radio.Rx(0);
+  }
+  Radio.IrqProcess( );
+
+  //Inserção dos comandos de avanço e recuo dos motores
+  // Avanço
+  /*if (com_avancar == 1) {
+    digitalWrite(MOTOR_PIN1, LOW);
+    digitalWrite(MOTOR_PIN2, HIGH);
+
+    Serial.printf("Avancando \n");
+  }
+
+  //Recuo
+  if (com_recuar == 1) {
+    digitalWrite(MOTOR_PIN1, HIGH);
+    digitalWrite(MOTOR_PIN2, LOW);
+
+    Serial.printf("Recuando \n");
+  }
+
+  if(com_recuar == 0 && com_avancar == 0){
+    digitalWrite(MOTOR_PIN1, LOW);
+    digitalWrite(MOTOR_PIN2, LOW);
+  }*/
+}
+
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
+{
+    rssi=rssi;
+    rxSize=size;
+    memcpy(rxpacket, payload, size );
+    rxpacket[size]='\0';
+    Radio.Sleep( );
+    Serial.printf("\r\nreceived packet \"%s\" with rssi %d , length %d\r\n",rxpacket,rssi,rxSize);
+    
+    if (strcmp(rxpacket, "Recuar") == 0) {
+      com_recuar = 1;
+    }
+    else{
+      com_recuar = 0;
+    }
+
+    if (strcmp(rxpacket, "Avancar") == 0) {
+      com_avancar = 1;
+    }
+    else{
+      com_avancar = 0;
+    }
+
+    //Eletroima
+    if (strcmp(rxpacket, "A") == 0) {
+        /*digitalWrite(AUTOMACAO_PIN1, HIGH);
+        digitalWrite(AUTOMACAO_PIN2, LOW);
+
+        delay(2500);
+
+        digitalWrite(AUTOMACAO_PIN1, LOW);
+        digitalWrite(AUTOMACAO_PIN2, HIGH);
+
+        delay(2250);
+
+        digitalWrite(AUTOMACAO_PIN1, LOW);
+        digitalWrite(AUTOMACAO_PIN2, LOW);*/
+
+        Serial.printf("Eletroima \n");  
+    }  
+    
+    //Automacao
+    if (strcmp(rxpacket, "C") == 0) {
+        digitalWrite(MOTOR_A_PIN1, HIGH);
+        digitalWrite(MOTOR_A_PIN2, LOW);
+
+        delay(10000);
+
+        digitalWrite(MOTOR_A_PIN1, LOW);
+        digitalWrite(MOTOR_A_PIN2, LOW);
+
+        delay(1000);
+
+        digitalWrite(MOTOR_B_PIN1, HIGH);
+        digitalWrite(MOTOR_B_PIN2, LOW);
+
+        delay(10000);
+
+        digitalWrite(MOTOR_B_PIN1, LOW);
+        digitalWrite(MOTOR_B_PIN2, LOW);
+
+        delay(1000);
+
+        digitalWrite(MOTOR_C_PIN1, HIGH);
+        digitalWrite(MOTOR_C_PIN2, LOW);
+
+        delay(3000);
+
+        digitalWrite(MOTOR_C_PIN1, LOW);
+        digitalWrite(MOTOR_C_PIN2, LOW);
+
+        delay(1000);
+
+        digitalWrite(MOTOR_D_PIN1, HIGH);
+        digitalWrite(MOTOR_D_PIN2, LOW);
+
+        delay(3000);
+
+        digitalWrite(MOTOR_D_PIN1, LOW);
+        digitalWrite(MOTOR_D_PIN2, LOW);
+
+        delay(5000);
+
+        //Volta
+        digitalWrite(MOTOR_A_PIN1, LOW);
+        digitalWrite(MOTOR_A_PIN2, HIGH);
+
+        delay(10000);
+
+        digitalWrite(MOTOR_A_PIN1, LOW);
+        digitalWrite(MOTOR_A_PIN2, LOW);
+
+        delay(1000);
+
+        digitalWrite(MOTOR_B_PIN1, LOW);
+        digitalWrite(MOTOR_B_PIN2, HIGH);
+
+        delay(5000);
+
+        digitalWrite(MOTOR_B_PIN1, LOW);
+        digitalWrite(MOTOR_B_PIN2, LOW);
+
+        delay(1000);
+
+        digitalWrite(MOTOR_C_PIN1, LOW);
+        digitalWrite(MOTOR_C_PIN2, HIGH);
+
+        delay(3000);
+
+        digitalWrite(MOTOR_C_PIN1, LOW);
+        digitalWrite(MOTOR_C_PIN2, LOW);
+
+        delay(1000);
+
+        digitalWrite(MOTOR_D_PIN1, LOW);
+        digitalWrite(MOTOR_D_PIN2, HIGH);
+
+        delay(3000);
+
+        digitalWrite(MOTOR_D_PIN1, LOW);
+        digitalWrite(MOTOR_D_PIN2, LOW);
+
+        Serial.printf("Automacao \n");  
+    }  
+    
+    lora_idle = true;
+}
